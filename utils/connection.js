fb9357d3cc11d8303cf9c188b22132a6022686e8
Lorenzo Aceti <lorenzo.acetii@gmail.com>
2022-01-19 00:59:57 -0300

    Modificado página de login e links

--- utils/connection.js
+++ utils/connection.js
@@ -2,7 +2,8 @@ const knex = require('knex')({
     client: 'mysql',
     connection: process.env.DATABASE_URL,
     asyncStackTraces: false,
-    debug: false
+    debug: false,
+    pool: { min: 2, max: 5 }
 });
 
 module.exports = knex;
